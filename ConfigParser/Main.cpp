95990a0c3907b85c23a4301d4d08d4ed427f13b6
Dennis <denniscorvers@hotmail.com>
2020-08-13 03:05:50 +0200

    Added more type safety for template functions

--- ConfigParser/Main.cpp
+++ ConfigParser/Main.cpp
@@ -4,14 +4,14 @@ int main()
 {
 	Configuration config("ServerSettings.ini");
 
+	//Create, then register
 	auto ServerNameConf = StringProperty::create("Server Name", "Unknown", "Server to connect to.");
-	auto ServerPortConf = IntProperty::create("Server Port", 12345, 1000, 65535, "Port of the server.");
-	auto ConnectionLifetimeConf = FloatProperty::create("Lifetime", 10.5f, "Lifetime of the connection in minutes.");
-	ConnectionLifetimeConf->setMinValue(5.f);
-
 	config.registerProperty(ServerNameConf);
-	config.registerProperty(ServerPortConf);
-	config.registerProperty(ConnectionLifetimeConf);
+
+	//Create and register
+	auto ServerPortConf = config.registerProperty<IntProperty>("Server Port", 12345, 1000, 65535, "Port of the server.");
+	auto ConnectionLifetimeConf = config.registerProperty<FloatProperty>("Lifetime", 10.5f, "Lifetime of the connection in minutes.");
+	ConnectionLifetimeConf->setMinValue(5.f);
 
 	config.save();
 
